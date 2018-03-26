db831f57baecdc528b1ab5fcb09ea04366a8e61f
group6Leader <janda2@naver.com>
2018-03-26 10:48:20 +0900

    쓸데없는 프로젝트 삭제

--- SECURI~1/src/main/java/net/javavideotutorials/security/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package net.javavideotutorials.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer 
-{
-
-}
